43ac863fb6c9534bb706d7169645b5d1a713c6c4
DonkeyBoard <119456795+WillDenby@users.noreply.github.com>
2024-01-30 15:38:06 +0000

    structuring the package finding

--- pypip/main.py
+++ pypip/main.py
@@ -7,6 +7,9 @@ import ast
 import toml
 from hashlib import sha256
 from typing_extensions import Annotated, Optional
+from packaging.requirements import Requirement
+from packaging.version import Version
+from packaging.specifiers import SpecifierSet
 
 app = typer.Typer()
 
@@ -30,7 +33,14 @@ def find_requirements_txt(file_in):
                         required_packages.add(package)
             break  # Stop searching once requirements.txt is found
         current_dir = os.path.dirname(current_dir)  # Move to the parent directory
-    return list(required_packages)
+    print("Requirement.txt packages: ", required_packages)
+    return required_packages
+
+def find_latest_compatible_version(package, version):
+    req = Requirement(package + version)
+    specifier = SpecifierSet(','.join(map(str, req.specifier))) if req.specifier else None
+    latest_version = max(Version(p.version) for p in req.specifier.filter(Version))
+    return str(latest_version) if not specifier or latest_version in specifier else None
 
 def find_pyproject_toml(file_in):
     required_packages = set()  # Using a set to avoid duplicate entries
@@ -41,38 +51,50 @@ def find_pyproject_toml(file_in):
             with open(pyproject_toml_file, 'r') as f:
                 pyproject_toml_data = toml.load(f)
                 # Extract dependencies from the 'tool.poetry.dependencies' section
-            dependencies = pyproject_toml_data.get('tool', {}).get('poetry', {}).get('dependencies', {})
-            for package, version in dependencies.items():
-                required_packages.add(f"{package}=={version}")
+                dependencies = pyproject_toml_data.get('tool', {}).get('poetry', {}).get('dependencies', {})
+                for package, version in dependencies.items():
+                    updated_version = find_latest_compatible_version(package, version)
+                    if updated_version:
+                        required_packages.add(f"{package}=={updated_version}")
+
         current_dir = os.path.dirname(current_dir)  # Move to the parent directory
-    
-    return list(required_packages)  # Return the list of required packages
+    print("Pyproject.toml packages: ", required_packages)
+    return required_packages
 
 def find_imports(file_in):
-    imports = []
+    imports = set()
     with open(file_in, 'r') as file:
         tree = ast.parse(file.read(), filename=file_in)
         for node in ast.walk(tree):
             if isinstance(node, ast.Import):
                 for alias in node.names:
-                    imports.append(alias.name.split('.')[0])  # Take only the master package
+                    imports.add(alias.name.split('.')[0])  # Take only the master package
             elif isinstance(node, ast.ImportFrom):
                 if node.module is not None:
-                    imports.append(node.module.split('.')[0])  # Take only the master package
+                    imports.add(node.module.split('.')[0])  # Take only the master package
+    print("Import statement packages: ", imports)                    
     return imports
 
-def find_packages(file_in):
-    package_names = []
-    package_names.extend(find_requirements_txt(file_in))
-    package_names.extend(find_pyproject_toml(file_in))
-    package_names.extend(find_imports(file_in))
-    return package_names
+def find_required_packages(file_in):
+    required_packages = set()
+    imports = find_imports(file_in)
+    required_packages.update(imports)
+    requirements_packages = find_requirements_txt(file_in) or find_pyproject_toml(file_in)
+    unversioned_requirements_packages = set()
+    for item in requirements_packages:
+        package_name = item.split("==")[0]
+        unversioned_requirements_packages.add(package_name)
+    required_packages -= required_packages.intersection(unversioned_requirements_packages)
+    required_packages.update(requirements_packages)
+    print("Neutralised set of all the packages :", required_packages )
+    return required_packages
 
-def check_packages(venv_path, package_names):
+def find_existing_packages(venv_path):
     result = subprocess.run([os.path.join(venv_path, 'bin', 'pip'), 'freeze'], capture_output=True, text=True)
-    installed_packages = [line.split()[0] for line in result.stdout.splitlines()[2:]]
-    missing_packages = [pkg for pkg in package_names if pkg not in installed_packages]
-    return missing_packages
+    existing_packages = set(result.stdout.strip().split('\n'))
+    return existing_packages
+
+
 
 def install_packages(venv_path, missing_packages):
     with open(os.devnull, 'w') as devnull:
@@ -102,9 +124,11 @@ def main(file: Path,
     if not os.path.exists(venv_path):
         subprocess.run([sys.executable, '-m', 'virtualenv', venv_path])
         
-    package_names = find_packages(file)
-    missing_packages = check_packages(venv_path, package_names)
-
+    required_packages = find_required_packages(file)
+    existing_packages = find_existing_packages(venv_path)
+    print("Existing packages :", existing_packages)
+    missing_packages = required_packages - existing_packages  
+    print("Missing packages :", missing_packages)
     install_packages(venv_path, missing_packages)
     run_script(venv_path, file, ctx.args)
     
